c0059998fd42536cdabae01adef02a005f94b167
Dionte Driskell <driskell06@gmail.com>
2024-06-23 15:06:58 -0400

    added imports and exports for the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from '../features/productsSlice';
 import cartReducer from '../features/cartSlice';
+//added imports
 
+//exporting to be on the store
 export const store = configureStore({
   reducer: {
     products: productsReducer,
